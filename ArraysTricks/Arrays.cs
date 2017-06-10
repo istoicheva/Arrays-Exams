a11c6a1c216ee42287fc87f2f16f069e35f0cf49
istoicheva <ina.stoicheva@gmail.com>
2017-06-10 21:56:53 +0300

    Add files via upload

---
+++ ArraysTricks/Arrays.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraysTricks
+{
+    class Arrays
+    {
+        static void Main(string[] args)
+        {
+            int n = 4;
+            int number = 0;
+            int[,] theArray = new int[n, n];
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = n - 1; j > 0;)
+                {
+                    while (true)
+                    {
+                        if (theArray[i,j] == 0)
+                        {
+                            theArray[i, j] = number + 1;
+                            number += 1;
+                            break;
+                        }
+                        j--;
+                    }
+                }
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write(theArray[i,j]);
+                }
+                Console.WriteLine();
+            }
+        }
+   }
+}
