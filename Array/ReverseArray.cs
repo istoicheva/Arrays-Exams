aa972b58eb3cf335d39f5394217ec5f7f6d25f9c
istoicheva <ina.stoicheva@gmail.com>
2017-06-10 21:57:24 +0300

    Add files via upload

---
+++ Array/ReverseArray.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array
+{
+    class ReverseArray
+    {
+        static void Main(string[] args)
+        {
+            int[] array = {1, 2, 3, 4, 5};
+            int length = array.Length;  // Get array length
+            int[] reverse = new int[length];    // Declare and create the reversed array
+
+            // Initiate the reverse array
+            for (int index = 0; index < length; index++)
+            {
+                reverse[length - index - 1] = array[index];
+            }
+
+            //Print the reverse array
+            for (int index = 0; index < length; index++)
+            {
+                Console.Write(reverse[index] + " ");
+            }
+        }
+    }
+}
