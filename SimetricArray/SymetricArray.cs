ba3d0154acc244cb51e059091376bda26cfb3246
istoicheva <ina.stoicheva@gmail.com>
2017-06-10 21:59:36 +0300

    Add files via upload

---
+++ SimetricArray/SymetricArray.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimetricArray
+{
+    class SymmetricArray
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a positive integer: ");
+            int array_length = Int32.Parse(Console.ReadLine());
+            int[] array = new int[array_length];
+
+            Console.WriteLine("Enter the value of the array:");
+
+            for (int i = 0; i < array_length; i++)
+            {
+                array[i] = Int32.Parse(Console.ReadLine());
+            }
+
+            bool symmetric = true;
+            for (int i = 0; i < array.Length / 2; i++)
+            {
+                if (array[i] != array[array_length - i - 1])
+                {
+                    symmetric = false;
+                }
+            }
+
+            Console.WriteLine("Is symmetric? {0}", symmetric);
+        }
+    }
+}
